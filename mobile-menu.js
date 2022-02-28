b72a958855e153175b15fbc9ed8b1279dcf8beb5
razvan <razvanges@yahoo.com>
2022-02-28 16:15:39 +0200

    Add basic structure for the mobile menu

---
+++ mobile-menu.js
@@ -0,0 +1,8 @@
+let menu = document.getElementById('menu')
+let button = document.getElementById('hamburger')
+
+button.addEventListener('click', e => {
+  console.log('click')
+  
+  menu.classList.add('visible-menu')
+})

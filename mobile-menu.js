a75b3d8a69547a6b6bd424b22f46df65ca5eebed
razvan <razvanges@yahoo.com>
2022-02-28 16:51:37 +0200

    Add new close button

--- mobile-menu.js
+++ mobile-menu.js
@@ -1,9 +1,11 @@
 let menu = document.getElementById('menu')
 let button = document.getElementById('hamburger')
+let buttonClose = document.getElementById('hamburger-close')
 
 button.addEventListener('click', e => {
   console.log('click')
 
   menu.classList.add('visible-menu')
   menu.classList.remove('hidden')
+
 })

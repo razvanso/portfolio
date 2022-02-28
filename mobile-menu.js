7a43c03e0d9b8e60477b01fcb4f261dce46e5766
razvan <razvanges@yahoo.com>
2022-02-28 16:43:02 +0200

    Fixed text and blur styling

--- mobile-menu.js
+++ mobile-menu.js
@@ -3,6 +3,7 @@ let button = document.getElementById('hamburger')
 
 button.addEventListener('click', e => {
   console.log('click')
-  
+
   menu.classList.add('visible-menu')
+  menu.classList.remove('hidden')
 })

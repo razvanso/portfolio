113e0963c7050b8c6cca6189cad4393555b3431d
razvan <razvanges@yahoo.com>
2022-03-05 16:58:38 +0200

    Hide the menu after clicking an item

--- mobile-menu.js
+++ mobile-menu.js
@@ -1,9 +1,16 @@
 let menu = document.getElementById('menu');
 let button = document.querySelector('.toggle-icon');
-let menuItems = document.querySelectorAll('.menu li');
+let menuItems = document.querySelectorAll('#menu li');
 
-button.addEventListener('click', e => {
+button.addEventListener('click', event => {
   button.classList.toggle('active');
   menu.classList.toggle('active');
 });
 
+menuItems.forEach(item => {
+  item.addEventListener('click', event => {
+    console.log('hide');
+    button.classList.remove('active');
+    menu.classList.remove('active');
+  });
+});

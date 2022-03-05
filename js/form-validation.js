c7161d094602fd10f9a4c5b3e6f425c5364127cd
razvan <razvanges@yahoo.com>
2022-03-06 00:45:13 +0200

    Add persistent storage

--- js/form-validation.js
+++ js/form-validation.js
@@ -2,6 +2,12 @@ const emailErrorMessage = 'Please enter email address in small letters.';
 const contactForm = document.getElementById('contact-form');
 const emailInput = document.getElementById('email');
 const errorMessageContainer = document.querySelector('.error-message');
+const formElement = document.querySelectorAll('#name, #email, #message');
+let formData = { name: '', email: '', message: '' };
+if (localStorage.getItem('formdata') !== null) {
+  formData = JSON.parse(localStorage.getItem('formdata'));
+}
+
 
 const validateEmail = () => {
   const lowercaseEmail = emailInput.value.toLowerCase();
@@ -18,5 +24,15 @@ const validateEmail = () => {
 contactForm.addEventListener('submit', (event) => {
   if (!validateEmail()) {
     event.preventDefault();
+  } else {
+    localStorage.removeItem('formdata');
   }
+});
+
+formElement.forEach((formItem) => {
+  formItem.value = formData[formItem.name];
+  formItem.addEventListener('input', (e) => {
+    formData[e.target.name] = e.target.value;
+    localStorage.setItem('formdata', JSON.stringify(formData));
+  });
 });
